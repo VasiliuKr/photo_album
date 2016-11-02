f2d283af4698381cd7d1938c348b09d173573874
Elizaveta <eovinova89@gmail.com>
2016-11-03 01:52:29 +0300

    Создание ветки

--- source/js/page_template.js
+++ source/js/page_template.js
@@ -14,3 +14,25 @@ var pageTemplate = (function() {
     init: init
   };
 }());
+
+
+var template_base={
+  photo_main:{
+    template:'main',
+    photos:{
+      ajax_url:'/ajax/main/',
+      box:'.photo-albums__list'
+    },
+    album:false
+  }
+};
+
+data={tempate:'photo_main',params:''};
+
+if (!template_base[data.tempate]) {
+  false;
+}
+
+t=template_base[data.tempate]
+
+t.photos.ajax_url+params
