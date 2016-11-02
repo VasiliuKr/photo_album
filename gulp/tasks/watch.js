1e389fa91e50dba647c1ba792348caa298d2167b
Ivan Korepanov <moskovanin@yandex.ru>
2016-11-02 11:09:06 +0300

    Исправлена ошибка js, добавлена анимация появления модалки

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,8 +2,8 @@
 
 module.exports = function() {
   $.gulp.task('watch', function() {
-    $.gulp.watch('./source/js/**/*.js', $.gulp.series([/*'js:lint',*/'js:process']));
-    $.gulp.watch('./source/style/**/*.scss', $.gulp.series([/*'sass.lint',*/'sass']));
+    $.gulp.watch('./source/js/**/*.js', $.gulp.series(['js:lint','js:process']));
+    $.gulp.watch('./source/style/**/*.scss', $.gulp.series(['sass.lint','sass']));
     $.gulp.watch('./source/template/**/*.pug', $.gulp.series(['frontend.jade','pug']));
     $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
     $.gulp.watch('./source/sprite/**/*.svg', $.gulp.series('sprite:svg'));
