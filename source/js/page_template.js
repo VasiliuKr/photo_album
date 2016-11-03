e6c91c94458bbc621bc53192402749349e20aa70
Elizaveta <eovinova89@gmail.com>
2016-11-03 14:40:00 +0300

    Шаблон главной страницы

--- source/js/page_template.js
+++ source/js/page_template.js
@@ -1,6 +1,22 @@
 'use strict';
 
 var pageTemplate = (function() {
+  var template__base = {
+    main: {
+      content_template: 'content_main',
+      photos: {
+        ajax_url:'/ajax/main/',
+        box: '.photo-albums__list',
+        canEdit: false
+      },
+      album: {
+        ajax_url:'/ajax/main/',
+        box: '.my-albums__list',
+        canEdit: true
+      }
+    }
+  }
+
   var init = function(updateFunction) {
 
   };
@@ -9,30 +25,38 @@ var pageTemplate = (function() {
     return true;
   };
 
+  var template = data.template.replace('.html', '');
+
+  if (!template__base[template]) return false;
+
+  template = template__base[template];
+  var content_template = template['content_template'];
+  content_template = templates[content_template]();
+
+  $('#content').html(content_template);
+
+  var _functionAdd = function(data,url_param,callbackFunction) {
+    url=data.ajax_url+url_param,
+    /*$.post(url,function(djson){
+     callbackFunction(djson,data.box)
+     },'json')*/
+    callbackFunction({},data.box,data.canEdit)
+  };
+
+  if(template.photos){
+    _functionAdd(template.photos,data.data,photo.set);
+   }
+
+  if(template.album) {
+    _functionAdd(template.album, data.data, album.set);
+  }
+
   return {
     update: update,
     init: init
   };
+  
 }());
 
 
-var template_base={
-  photo_main:{
-    template:'main',
-    photos:{
-      ajax_url:'/ajax/main/',
-      box:'.photo-albums__list'
-    },
-    album:false
-  }
-};
 
-data={tempate:'photo_main',params:''};
-
-if (!template_base[data.tempate]) {
-  false;
-}
-
-t=template_base[data.tempate]
-
-t.photos.ajax_url+params
